06c5431c2b18b718e556128161e401a640887c6a
Diobreque <117220996+Diobreque@users.noreply.github.com>
2023-11-28 18:24:40 -0300

    sisi

--- registro_socios/registro_socios/settings.py
+++ registro_socios/registro_socios/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -76,8 +77,8 @@ WSGI_APPLICATION = 'registro_socios.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'DJANGO_SOCIOS',
     }
 }
 
--- registro_socios/socios_app/models.py
+++ registro_socios/socios_app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+# • Para el sistema se necesitará almacenar, el nombre del socio, fecha incorporación, año de
+# nacimiento, teléfono, correo electrónico, sexo, estado (Vigente, Suspendido, Retirado),
+# observacion
+# • Se debe implementar el CRUD completo utilizando funciones.
+# • Todos los campos son obligatorios, excepto la observación, la cantidad de personas debe
+# estar entre 1 y 15.
+# Todos los campos son obligatorios, excepto la observación, la cantidad de personas debe
+# estar entre 1 y 15, validación de correo electrónico, el nombre del socio no puede superar
+# los 80 caracteres.
+
+selector_sexo = [(1,'Femenino'),(2,'Masculino')(3,'Otro')]
+
+class Socios(models.Model):
+    id_socio = models.AutoField(primary_key=True)
+    nombre_socio = models.CharField(max_length=80)
+    fecha_incorporacion = models.DateField()
+    anio_nacimiento = models.DateField()
+    telefono = models.IntegerField()
+    correo_electronico = models.DateField()
+    sexo = models.IntegerField(
+        null=False, blank=False,
+        choices=selector_sexo
+    )
+    def __str__(self):
+        return self.nombre_socio
